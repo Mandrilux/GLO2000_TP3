0c79d9fd8e2be9d57d03b2ae6c917b87d01e7e48
heraud_b <baptiste.heraud@epitech.eu>
2018-10-19 18:05:42 -0400

    easy test

--- TP3-Q1.py
+++ TP3-Q1.py
@@ -12,8 +12,27 @@ except ImportError:
     sys.exit(84)
 
 
-def runServer():
-    print("on lance le serveur")
+def runServer(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_address = ("0.0.0.0", port)
+    print('starting up on {} port {}'.format(*server_address))
+    sock.bind(server_address)
+    sock.listen(1)
+    while True:
+        print('waiting for a connection')
+        connection, client_address = sock.accept()
+        try:
+            print('connection from', client_address)
+            while True:
+
+                data = connection.recv(1024).decode("utf-8").replace("\r\n", "")
+                print (data)
+                if not data:
+                    break
+            """connection.sendall(str.encode("Bien recu"), 0)"""
+        finally:
+            connection.close()
 
 def runClient():
     print("on run le client")
@@ -28,6 +47,7 @@ if __name__ == "__main__":
     parser.add_argument("--serveur", "-s", help="start a server (client mode by default)", action="store_true")
     parser.add_argument("--destination", "-d", help="setup Ip destination ( with client mode only)")
     args = parser.parse_args()
+    port = args.port
 
     if args.serveur:
         server = 1
@@ -40,29 +60,8 @@ if __name__ == "__main__":
         sys.exit(84)
 
     if server:
-        runServer()
+        runServer(port)
     else:
         runClient()
-    """sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    server_address = ('localhost', port)
-    print('starting up on {} port {}'.format(*server_address))
-    sock.bind(server_address)
-
-    sock.listen(1)
-
-    while True:
-
-        print('waiting for a connection')
-        connection, client_address = sock.accept()
-        try:
-            print('connection from', client_address)
-            while True:
-                data = connection.recv(1024).decode("utf-8").replace("\r\n", "")
-                print (data)
-                if not data:
-                    break
-
-        finally:
-            connection.close()"""
     sys.exit(0)
