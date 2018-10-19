afae154299467434b2b7f5761fe0ab76e8c4b0cb
heraud_b <baptiste.heraud@epitech.eu>
2018-10-19 17:16:25 -0400

    fix error

--- TP3-Q1.py
+++ TP3-Q1.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: latin-1 -*-
 
-import argparse
-import socket
 
+import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--port", action="store", dest="port", type=int, default=1337)
-parser.add_argument("-s", "--serveur", action="store", dest="serveur", type=int, default=0)
-port = vars(parser.parse_args())["port"]
-flagS = vars(parser.parse_args())["serveur"]
+try:
+    import argparse
+    import socket
+except ImportError:
+    print("Error ! can not loaded external libs", file=sys.stderr)
+    sys.exit(84)
 
-print (flagS)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", action="store", dest="port", type=int, default=1337)
+    port = vars(parser.parse_args())["port"]
 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', port)
+    print('starting up on {} port {}'.format(*server_address))
+    sock.bind(server_address)
 
-server_address = ('localhost', port)
-print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+    sock.listen(1)
 
-sock.listen(1)
+    while True:
 
-while True:
+        print('waiting for a connection')
+        connection, client_address = sock.accept()
+        try:
+            print('connection from', client_address)
+            while True:
+                data = connection.recv(1024).decode("utf-8").replace("\r\n", "")
+                print (data)
+                if not data:
+                    break
 
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
-    try:
-        print('connection from', client_address)
-        while True:
-            data = connection.recv(1024).decode("utf-8").replace("\r\n", "")
-            print (data)
-            if not data:
-                break
-
-    finally:
-        connection.close()
+        finally:
+            connection.close()
