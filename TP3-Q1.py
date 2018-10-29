06a383ed3e73bcc2768f45fc0560d257f1310bf7
heraud_b <baptiste.heraud@epitech.eu>
2018-10-29 14:55:14 -0400

    Generation de nombre premier

--- TP3-Q1.py
+++ TP3-Q1.py
@@ -7,6 +7,7 @@ import sys
 try:
     import argparse
     import socket
+    from cryptoModule import entierAleatoire, trouverNombrePremier, exponentiationModulaire
 except ImportError:
     print("Error ! can not loaded external libs", file=sys.stderr)
     sys.exit(84)
@@ -14,18 +15,21 @@ except ImportError:
 
 def runServer(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     server_address = ("0.0.0.0", port)
     print('starting up on {} port {}'.format(*server_address))
     sock.bind(server_address)
     sock.listen(1)
     while True:
         print('waiting for a connection')
+        m = entierAleatoire(1000)
+        print (m)
+        n = trouverNombrePremier()
+        print (n)
         connection, client_address = sock.accept()
         try:
             print('connection from', client_address)
-            while True:
 
+            while True:
                 data = connection.recv(1024).decode("utf-8").replace("\r\n", "")
                 print (data)
                 if not data:
